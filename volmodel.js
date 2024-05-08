f0f88062e8c58948c444bf99b6f326317b6dbaa0
VladITKid <vladitkid@gmail.com>
2024-05-08 11:52:47 +0300

    Меняю стили для левых и правых конечностей

--- volmodel.js
+++ volmodel.js
@@ -1,6 +1,7 @@
-// Получаем все элементы модели
+// Получаем все 3D модели
 const volmodels = document.querySelectorAll('.volmodel');
 
+// todo: Сделать поворачивение 3D vоделей пользователем
 // Переменные для хранения начальной позиции и угла
 let startX = 0;
 let startY = 0;
@@ -49,3 +50,37 @@ volmodels.forEach(volmodel => {
     volmodel.addEventListener('mousedown', handleDragStart);
     volmodel.addEventListener('touchstart', handleDragStart);
 });
+
+// todo: Добавлять модификатор анимации
+function addAnimationModifer(modiferName) {
+    // Функция добавляет модификатор ко всем классам элемента
+    function addClassWithModifer(element, modiferName) {
+        // Получаем все классы элемента
+        const classes = element.className.split(' ');
+    
+        // Добавляем модификатор к каждому классу
+        classes.forEach((className) => {
+            if (className.startsWith('volhuman__')) { // Проверяем, начинается ли класс с нужного префикса
+                element.classList.add(`${className}--${modiferName}`);
+            }
+        });
+    }
+    // Добавляем модификатор ко всем элементам
+    addClassWithModifer(head, modiferName);
+    addClassWithModifer(body, modiferName);
+    addClassWithModifer(handLeft, modiferName);
+    addClassWithModifer(handRight, modiferName);
+    addClassWithModifer(legLeft, modiferName);
+    addClassWithModifer(legRight, modiferName);
+}
+
+// Меши 3D человека
+const head = document.querySelector('.volhuman__head');
+const body = document.querySelector('.volhuman__body');
+const handLeft = document.querySelector('.volhuman__hand--left');
+const handRight = document.querySelector('.volhuman__hand--right');
+const legLeft = document.querySelector('.volhuman__leg--left');
+const legRight = document.querySelector('.volhuman__leg--right');
+
+// Применяю функцию анимации
+addAnimationModifer('walking');
